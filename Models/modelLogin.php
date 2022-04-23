f7d26db6dd537a192f240a57ea35e3bfc81440e6
Faber Hoyos <faberhoyos01@gmail.com>
2022-04-23 09:45:06 -0500

    Merge

--- Models/modelLogin.php
+++ Models/modelLogin.php
@@ -32,7 +32,7 @@ function Login($data)
 {
     $bd = connection();
     $encodepass = md5($data->password);
-    $sentencia = $bd->prepare("SELECT * FROM usuarios");
+    $sentencia = $bd->prepare("SELECT * FROM usuarios WHERE email = ? AND passw = ?");
     $sentencia->execute([$data->email, $encodepass]);
     return $sentencia->fetchObject();
 }
