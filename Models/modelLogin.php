da3bed05e9d550dd4f67869ccff0a3a47d471829
Faber Hoyos <faberhoyos01@gmail.com>
2022-04-23 09:26:02 -0500

    conexion a heroku

--- Models/modelLogin.php
+++ Models/modelLogin.php
@@ -32,7 +32,7 @@ function Login($data)
 {
     $bd = connection();
     $encodepass = md5($data->password);
-    $sentencia = $bd->prepare("SELECT email, names, lastnames, rol FROM usuarios where email = ? and passw = ?");
+    $sentencia = $bd->prepare("select * from user_away");
     $sentencia->execute([$data->email, $encodepass]);
     return $sentencia->fetchObject();
 }
@@ -42,7 +42,7 @@ function connection()
     $pass = "ce8ba12b90601ee125a4e41ccc55983fab6e4898f76948b5c5f957515e1f6d09";
     $user = "btrkioumjjkibb";
     $dbName = "d7jecr30ej3csa";
-    $database = new PDO('psql:host=127.ec2-34-192-210-139.compute-1.amazonaws.com;dbname=' . $dbName, $user, $pass);
+    $database = new PDO("pgsql:host=ec2-34-192-210-139.compute-1.amazonaws.com;dbname=". $dbName, $user, $pass);
     $database->query("set names utf8;");
     $database->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
     $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
